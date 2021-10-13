b9e033985d76bb1e52631a3067b91d818e134d4d
Mai Thành Duy An <tiachop0102@gmail.com>
2021-10-13 23:36:31 +0900

    First commit. Create new NodeJS project.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ctb
+
 ctb
+
+### API KEY
+
+[BINANCE API KEY](https://testnet.binance.vision/key/generate)
+
+### LIBRARY
+
+[CCXT](https://www.npmjs.com/package/ccxt)
---
+++ index.js
@@ -0,0 +1,71 @@
+//console.log("Hello World !");
+
+const ccxt = require("ccxt");
+const moment = require("moment");
+const delay = require("delay");
+require("dotenv").config();
+
+const binance = new ccxt.binance({
+  apiKey: process.env.API_KEY,
+  secret: process.env.SECRET,
+});
+
+// For demo account
+binance.setSandboxMode(true);
+
+async function printBalance(btcPrice) {
+  const balance = await binance.fetchBalance();
+  const total = balance.total;
+  console.log(`Balance : BTC ${total.BTC}, USDT: ${total.USDT}`);
+  console.log(`Total USD: ${(total.BTC - 1) * btcPrice + total.USDT}. \n`);
+}
+
+async function tick() {
+  const prices = await binance.fetchOHLCV("BTC/USDT", "1m", undefined, 5);
+  const bPrice = prices.map((price) => {
+    return {
+      timestamp: moment(price[0]).format(),
+      open: price[1],
+      high: price[2],
+      low: price[3],
+      close: price[4],
+      volume: price[5],
+    };
+  });
+  const averagePrice = bPrice.reduce((acc, price) => acc + price.close, 0) / 5;
+  const lastPrice = bPrice[bPrice.length - 1].close;
+  //   console.log(bPrice);
+  console.log(
+    bPrice.map((p) => p.close),
+    averagePrice,
+    lastPrice
+  );
+
+  //   Algo Trading
+  const direction = lastPrice > averagePrice ? "sell" : "buy";
+  const TRADE_SIZE = 100;
+  const quantity = TRADE_SIZE / lastPrice;
+
+  console.log(`Average Price: ${averagePrice}. LastPrice: ${lastPrice}`);
+  const order = await binance.createMarketOrder(
+    "BTC/USDT",
+    direction,
+    quantity
+  );
+
+  console.log(
+    `${moment().format()}: ${direction} ${quantity} BTC at ${lastPrice}`
+  );
+  console.log(order);
+  printBalance(lastPrice);
+}
+
+async function main() {
+  while (true) {
+    await tick();
+    await delay(60 * 1000);
+  }
+}
+
+// printBalance();
+main();
